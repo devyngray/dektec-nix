595f44fb177b784c4c2df071e6cc6e66bbbbe462
Devyn Gray <devyngray@proton.me>
2025-11-19 13:23:27 -0500

    have nixos module not specify kernel version

--- modules/dektec-dtu.nix
+++ modules/dektec-dtu.nix
@@ -6,12 +6,13 @@
 }:
 let
   cfg = config.hardware.dektec-dtu;
+  dektec-dtu-kmod = pkgs.dektec-dtu-kmodFor config.boot.kernelPackages;
 in
 {
   options.hardware.dektec-dtu.enable = lib.mkEnableOption "DekTec DTU drivers and userspace";
 
   config = lib.mkIf cfg.enable {
-    boot.extraModulePackages = [ pkgs.dektec-dtu-kmod ];
+    boot.extraModulePackages = [ dektec-dtu-kmod ];
 
     boot.kernelModules = [ "Dtu" ];
 
--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,7 @@
 final: prev: {
   dektec-linux-sdk = final.callPackage ./dektec-linux-sdk.nix { };
   dt-info-cl = final.callPackage ./dt-info-cl.nix { };
-  dektec-dtu-kmod = final.linuxPackages.callPackage ./dektec-dtu-kmod.nix { };
+
+  dektec-dtu-kmodFor = linuxPackages: linuxPackages.callPackage ./dektec-dtu-kmod.nix { };
+  dektec-dtu-kmod = final.dektec-dtu-kmodFor final.linuxPackages;
 }
